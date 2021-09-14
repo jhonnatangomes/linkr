6f1d50b97e039f97869a0f95b4a750d0d4bb990e
Adriano Gotardo <adrianogotardo.12@gmail.com>
2021-09-14 15:39:33 -0300

    Fixed rendering error on index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import './styles/reset.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.querySelector('.root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
