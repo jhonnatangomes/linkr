11e85fd57b7d8747c98f06638c5dcbabef3b3f52
Adriano Gotardo <adrianogotardo.12@gmail.com>
2021-09-15 13:57:38 -0300

    Initialized the layout implementation

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+
 export default function Timeline() {
     return(
-        <h1>TIMELINE ! ! !</h1>
+        <TimelineBox>
+            <HeaderBox>
+                <Logo></Logo>
+                <Menu></Menu>
+            </HeaderBox>
+            <h1>TIMELINE ! ! !</h1>
+        </TimelineBox>
     )
 }
+
+const TimelineBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: lightblue;
+    width: 100vw;
+    height: calc(100vh - 72px);
+    margin: 72px 0 0 0;
+`;
+
+const HeaderBox = styled.div`
+    width: 100vw;
+    height: 72px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 17px 0 28px;
+    background: #151515;
+    position: fixed;
+    top: 0;
+    left: 0;
+`;
+
+const Logo = styled.div`
+
+`;
+
+const Menu = styled.div`
+
+`;
