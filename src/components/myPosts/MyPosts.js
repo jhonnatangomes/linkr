e985a729e67b9cf589fc1916c7d0ae5cf0a76efc
Luis Felipe <l_felipelizeu@hotmail.com>
2021-09-16 16:52:03 -0300

    Created a file that contains the api request to get a user posts

--- src/components/myPosts/MyPosts.js
+++ src/components/myPosts/MyPosts.js
@@ -1,7 +1,6 @@
-import axios from "axios";
-
 import { useState, useEffect, useContext } from 'react';
 import UserContext from '../../contexts/UserContext.js';
+import { getUserPosts } from '../../services/userPostsApi.js';
 
 export default function MyPosts () {
     const { user } = useContext(UserContext);
@@ -10,13 +9,7 @@ export default function MyPosts () {
     console.log(myPosts);
 
     useEffect(() => {
-        const config = {
-            headers: {
-                Authorization: `Bearer ${user.token}`
-            }
-        };
-
-        axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${user.user.id}/posts`, config)
+        getUserPosts(user.user.id, user.token)
             .then((response) => setMyPosts(response.data.posts))
             .catch(() => alert("Ocorreu algum erro!"));
     },[]);
