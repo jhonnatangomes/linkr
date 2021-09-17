35cf67b25507b859de45fabc731ff62ab374fb75
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-09-16 22:54:04 -0300

    Error showing when server returns an error and showing message for when server responds with no posts

--- src/components/timeline/PostsList.js
+++ src/components/timeline/PostsList.js
@@ -2,19 +2,25 @@ import styled from "styled-components";
 import { getPosts } from "../../services/api.js";
 import UserContext from "../../contexts/UserContext.js";
 import Post from "../shared/Post.js";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useHistory } from "react-router";
 
 export default function PostsList({ posts, setPosts }) {
     const { user } = useContext(UserContext);
     const history = useHistory();
+    const [errorMessage, setErrorMessage] = useState("");
+    const [serverResponded, setServerResponded] = useState(false);
 
     useEffect(() => {
         if (user) {
             const request = getPosts(user.token);
             request.then((res) => {
                 setPosts(res.data.posts);
+                setServerResponded(true);
             });
+            request.catch(() => {
+                setErrorMessage("Houve uma falha ao obter os posts, por favor atualize a página");
+            })
         } else {
             alert("Você não está logado!");
             history.push("/");
@@ -23,9 +29,11 @@ export default function PostsList({ posts, setPosts }) {
 
     return (
         <Container $loading={!posts.length}>
-            {posts.length ? posts.map((post) => (
+            {serverResponded ? posts.map((post) => (
                 <Post post={post} key={post.id} />
-            )) : <Loading>Carregando...</Loading>}
+            )) : <Span invisible={errorMessage}>Carregando...</Span>}
+            {posts.length === 0 && serverResponded ? <Span>Nenhum post encontrado</Span> : ""}
+            {errorMessage ? <Span>{errorMessage}</Span> : ""}
         </Container>
     );
 }
@@ -39,8 +47,9 @@ const Container = styled.section`
     }
 `;
 
-const Loading = styled.span`
+const Span = styled.span`
     font-size: 30px;
     color: white;
     font-weight: 700;
+    display: ${({invisible}) => invisible ? "none" : "block"};
 `
